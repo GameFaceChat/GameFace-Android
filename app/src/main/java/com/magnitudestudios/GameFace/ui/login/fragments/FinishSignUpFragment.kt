167ce538abf3aa327635fb9dc38b20d2b71a0a41
Srihari Vishnu <srihari.vishnu@gmail.com>
2020-06-17 17:14:53 -0400

    add username filters

--- app/src/main/java/com/magnitudestudios/GameFace/ui/login/fragments/FinishSignUpFragment.kt
+++ app/src/main/java/com/magnitudestudios/GameFace/ui/login/fragments/FinishSignUpFragment.kt
@@ -8,6 +8,7 @@
 package com.magnitudestudios.GameFace.ui.login.fragments
 
 import android.os.Bundle
+import android.text.InputFilter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -20,6 +21,7 @@ import com.magnitudestudios.GameFace.R
 import com.magnitudestudios.GameFace.databinding.FragmentFinishSigningUpBinding
 import com.magnitudestudios.GameFace.pojo.Helper.Status
 import com.magnitudestudios.GameFace.ui.login.LoginViewModel
+import java.util.*
 
 class FinishSignUpFragment : Fragment() {
     private lateinit var binding: FragmentFinishSigningUpBinding
@@ -35,31 +37,54 @@ class FinishSignUpFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         binding.btnFinishSignup.setOnClickListener {
             if (validateDetails() && viewModel.usernameExists.value?.data == false) {
                 viewModel.createUser(binding.signupUsernameInput.text.toString(), binding.fullNameInput.text.toString(),
                  binding.bioInput.text.toString())
             }
         }
-        binding.profilePic.setOnClickListener {
 
+        binding.profilePic.setOnClickListener {
         }
+
         binding.signupUsernameInput.doAfterTextChanged {text ->
             if (validateDetails()) viewModel.userNameExists(text.toString())
         }
 
         viewModel.usernameExists.observe(viewLifecycleOwner, Observer {
-            if (it.status == Status.SUCCESS && it.data!!) binding.signupUsernameInput.error = "Username already exists!"
+            if (it.status == Status.SUCCESS && it.data!!) binding.signupUsernameInput.error = getString(R.string.username_exists)
         })
 
     }
 
     private fun validateDetails(): Boolean {
         var flag = true
-        if (binding.signupUsernameInput.text.toString().length < 6) {
-            binding.signupUsernameInput.error = getString(R.string.username_length)
-            flag = false
+        val usernameText = binding.signupUsernameInput.text.toString()
+        when {
+            usernameText.length < 6 -> {
+                binding.signupUsernameInput.error = getString(R.string.username_length)
+                flag = false
+            }
+            usernameText.contains(" ") -> {
+                binding.signupUsernameInput.error = getString(R.string.username_no_spaces)
+                flag = false
+            }
+            usernameSwearWords(usernameText) -> {
+                binding.signupUsernameInput.error = getString(R.string.username_bad_phrase)
+                flag = false
+            }
         }
+
         return flag
     }
+
+    private fun usernameSwearWords(username: String) : Boolean {
+        for (a in resources.getStringArray(R.array.array_bad_words)) {
+            if (username.contains(a)) return true
+        }
+        return false
+    }
+
+
 }
