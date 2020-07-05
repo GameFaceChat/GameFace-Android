c35355be5eb8c3cf0b52266c1b23b73334eeee8b
Srihari Vishnu <srihari.vishnu@gmail.com>
2020-07-05 16:37:06 -0400

    improve takePhoto for quality (#58)

--- app/src/main/java/com/magnitudestudios/GameFace/ui/takePhoto/TakePhotoFragment.kt
+++ app/src/main/java/com/magnitudestudios/GameFace/ui/takePhoto/TakePhotoFragment.kt
@@ -9,6 +9,7 @@ package com.magnitudestudios.GameFace.ui.takePhoto
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.DisplayMetrics
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.MotionEvent
@@ -29,8 +30,12 @@ import com.magnitudestudios.GameFace.R
 import com.magnitudestudios.GameFace.databinding.FragmentTakePhotoBinding
 import java.io.File
 import java.util.concurrent.Executor
+import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
 
 class TakePhotoFragment : Fragment(), CameraXConfig.Provider, Executor {
+    private val TAG = "TakePhotoFragment"
     private lateinit var bind: FragmentTakePhotoBinding
     private lateinit var cameraProviderFuture : ListenableFuture<ProcessCameraProvider>
 
@@ -50,16 +55,13 @@ class TakePhotoFragment : Fragment(), CameraXConfig.Provider, Executor {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        cameraProviderFuture.addListener(Runnable {
-            cameraProvider = cameraProviderFuture.get()
-            bindPreview(cameraProvider)
-        }, ContextCompat.getMainExecutor(requireContext()))
+        getCameraProvider()
 
         bind.switchCamera.setOnClickListener {
             orientation = if (orientation == CameraSelector.LENS_FACING_FRONT) CameraSelector.LENS_FACING_BACK
             else CameraSelector.LENS_FACING_FRONT
             try {
-                bindPreview(cameraProvider)
+                setUpCamera(cameraProvider)
             } catch (e: Exception) {Log.e("TakePhotoFragment", e.message, e)}
         }
 
@@ -86,11 +88,33 @@ class TakePhotoFragment : Fragment(), CameraXConfig.Provider, Executor {
         })
 
     }
-    private fun bindPreview(cameraProvider : ProcessCameraProvider) {
-        cameraProvider.unbindAll()
+
+    private fun getCameraProvider() {
+        cameraProviderFuture.addListener(Runnable {
+            cameraProvider = cameraProviderFuture.get()
+            setUpCamera(cameraProvider)
+        }, ContextCompat.getMainExecutor(requireContext()))
+    }
+
+    private fun setUpCamera(cameraProvider : ProcessCameraProvider) {
+        cameraProvider.unbindAll()      //Unbind all for now
+        // Get screen metrics used to setup camera for full screen resolution
+        val metrics = DisplayMetrics().also { bind.previewView.display.getRealMetrics(it) }
+        Log.d(TAG, "Screen metrics: ${metrics.widthPixels} x ${metrics.heightPixels}")
+
+        val screenAspectRatio = aspectRatio(metrics.widthPixels, metrics.heightPixels)
+        Log.d(TAG, "Preview aspect ratio: $screenAspectRatio")
+
+        val rotation = bind.previewView.display.rotation
+
         val preview : Preview = Preview.Builder()
+                .setTargetAspectRatio(screenAspectRatio)
+                .setTargetRotation(rotation)
                 .build()
+
         captureInstance = ImageCapture.Builder()
+                .setCaptureMode(ImageCapture.CAPTURE_MODE_MAXIMIZE_QUALITY)
+                .setTargetAspectRatio(screenAspectRatio)
                 .setTargetRotation(bind.previewView.display.rotation)
                 .build()
 
@@ -99,16 +123,28 @@ class TakePhotoFragment : Fragment(), CameraXConfig.Provider, Executor {
                 .build()
 
         preview.setSurfaceProvider(bind.previewView.createSurfaceProvider())
+        try {
 
-        camera = cameraProvider.bindToLifecycle(viewLifecycleOwner, cameraSelector!!, preview, captureInstance)
-        setUpTapToFocus()
+            camera = cameraProvider.bindToLifecycle(viewLifecycleOwner, cameraSelector!!, preview, captureInstance)
+            setUpTapToFocus()
+        }
+        catch (e: Exception) {
+            Log.e(TAG, "Camera setup failed", e)
+        }
     }
 
     private fun takePicture() {
         if (captureInstance == null) return
+        // Setup image capture metadata
+        val metadata = ImageCapture.Metadata().apply {
+            isReversedHorizontal = orientation == CameraSelector.LENS_FACING_FRONT
+        }
 
+        // Create output options object which contains file + metadata
         val savedFile = File.createTempFile("tempProfilePic", ".jpg", requireContext().cacheDir);
-        val outputFileOptions = ImageCapture.OutputFileOptions.Builder(savedFile).build()
+        val outputFileOptions = ImageCapture.OutputFileOptions.Builder(savedFile)
+                .setMetadata(metadata)
+                .build()
         captureInstance?.takePicture(outputFileOptions,this, object : ImageCapture.OnImageSavedCallback {
             override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
                 activity?.runOnUiThread {
@@ -124,6 +160,7 @@ class TakePhotoFragment : Fragment(), CameraXConfig.Provider, Executor {
             }
         })
     }
+
     @SuppressLint("ClickableViewAccessibility")
     private fun setUpTapToFocus() {
         if (camera == null || cameraSelector == null) return
@@ -150,6 +187,14 @@ class TakePhotoFragment : Fragment(), CameraXConfig.Provider, Executor {
 
     override fun execute(command: Runnable) {
         command.run()
+    }
+
+    private fun aspectRatio(width: Int, height: Int): Int {
+        val previewRatio = max(width, height).toDouble() / min(width, height)
+        if (abs(previewRatio - (4.0/3.0)) <= abs(previewRatio - (16.0/9.0))) {
+            return AspectRatio.RATIO_4_3
+        }
+        return AspectRatio.RATIO_16_9
     }
 
 
